83019a8f2e300530dc8e97b74cfe95e3a230047a
pranjalsinha1965 <pranjalsinha1965@gmail.com>
2025-09-29 17:49:42 +0530

    Prop-types

--- codeial-final-live/src/components/PostsList.js
+++ codeial-final-live/src/components/PostsList.js
@@ -1,71 +1,69 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-const PostsList = ({ posts }) => {
-  const [commentText, setCommentText] = useState('');
 
-  const handleCommentChange = (e) => setCommentText(e.target.value);
-
-  return (
-    <div className="posts-list">
-      {posts.map((post) => (
-        <div className="post-wrapper" key={post._id}>
-          <div className="post-header">
-            <div className="post-avatar">
-              <img
-                src={post.user.avatar || "https://cdn-icons-png.flaticon.com/512/3135/3135715.png"} 
-                alt="user-pic"
-              />
-              <div>
-                <span className="post-author">{post.user.name}</span>
-                <span className="post-time">a minute ago</span>
-              </div>
-            </div>
-
-            <div className="post-content">{post.content}</div>
-
-            <div className="post-actions">
-              <div className="post-like">
+class PostsList extends Component {
+  render() {s
+    const { posts } = this.props;
+    return (
+      <div className="posts-list">
+        {posts.map((post) => (
+          <div className="post-wrapper" key={post._id}>
+            <div className="post-header">
+              <div className="post-avatar">
                 <img
-                  src="https://cdn-icons-png.flaticon.com/512/1077/1077035.png"
-                  alt="likes-icon"
+                  src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
+                  alt="user-pic"
                 />
-                <span>{post.likes?.length || 0}</span>
-              </div>
-
-              <div className="post-comments-icon">
-                <img
-                  src="https://cdn-icons-png.flaticon.com/512/1380/1380338.png"
-                  alt="comments-icon"
-                />
-                <span>{post.comments?.length || 0}</span>
-              </div>
-            </div>
-
-            <div className="post-comment-box">
-              <input
-                placeholder="Start typing a comment"
-                value={commentText}
-                onChange={handleCommentChange}
-              />
-            </div>
-
-            <div className="post-comments-list">
-              {post.comments?.map((comment) => (
-                <div className="post-comments-item" key={comment._id}>
-                  <div className="post-comment-header">
-                    <span className="post-comment-author">{comment.user.name}</span>
-                    <span className="post-comment-time">a minute ago</span>
-                    <span className="post-comment-likes">{comment.likes?.length || 0}</span>
-                  </div>
-                  <div className="post-comment-content">{comment.content}</div>
+                <div>
+                  <span className="post-author">{post.user.name}</span>
+                  <span className="post-time">a minute ago</span>
                 </div>
-              ))}
+              </div>
+              <div className="post-content">{post.content}</div>
+
+              <div className="post-actions">
+                <div className="post-like">
+                  <img
+                    src="https://image.flaticon.com/icons/svg/1077/1077035.svg"
+                    alt="likes-icon"
+                  />
+                  <span>{post.likes.length}</span>
+                </div>
+
+                <div className="post-comments-icon">
+                  <img
+                    src="https://image.flaticon.com/icons/svg/1380/1380338.svg"
+                    alt="comments-icon"
+                  />
+                  <span>{post.comments.length}</span>
+                </div>
+              </div>
+              <div className="post-comment-box">
+                <input placeholder="Start typing a comment" />
+              </div>
+
+              <div className="post-comments-list">
+                <div className="post-comments-item">
+                  <div className="post-comment-header">
+                    <span className="post-comment-author">Bill</span>
+                    <span className="post-comment-time">a minute ago</span>
+                    <span className="post-comment-likes">22</span>
+                  </div>
+
+                  <div className="post-comment-content">Random comment</div>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
-    </div>
-  );
+        ))}
+      </div>
+    );
+  }
+}
+
+PostsList.propTypes = {
+    posts: PropTypes.array.isRequired, 
 };
 
 export default PostsList;
