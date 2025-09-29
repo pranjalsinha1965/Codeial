f28150a0f2e46f07537c5ca7bab207cb653d7a08
pranjalsinha1965 <pranjalsinha1965@gmail.com>
2025-09-29 14:52:41 +0530

    Added store

---
+++ codeial-final-live/src/components/test.js
@@ -0,0 +1,25 @@
+// rcc
+
+// import React, { Component } from 'react'
+
+// export default class test extends Component {
+//   render() {
+//     return (
+//       <div>test</div>
+//     )
+//   }
+// }
+
+// rsf
+
+//  import React from 'react';
+ 
+//  function test(props) {
+//     return (
+//         <div>
+            
+//         </div>
+//     );
+//  }
+ 
+//  export default test;
