8084666998e90061fd25838353efe7ad6ec23033
pranjalsinha1965 <pranjalsinha1965@gmail.com>
2025-09-30 16:53:24 +0530

    Added NavBar

--- codeial-final-live/src/actions/posts.js
+++ codeial-final-live/src/actions/posts.js
@@ -1,8 +1,9 @@
 import { UPDATE_POSTS } from './actionTypes';
 
 export function fetchPosts() {
-  return (dispatch) => {
-    const url = 'http://codeial.com:8000/api/v2/posts?page=1&limit=5';
+  return async (dispatch) => {
+    const url = await fetch('/api/v2/posts?page=1&limit=5')
+;
     fetch(url)
       .then((response) => {
         return response.json();
